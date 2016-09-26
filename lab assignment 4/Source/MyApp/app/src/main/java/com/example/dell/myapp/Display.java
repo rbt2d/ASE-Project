49b2282c7711f1b463a2724ac9e69db5a5f2061d
rbt2d <ramanakumarbingi@gmail.com>
2016-09-26 14:59:02 -0500

    update

---
+++ lab assignment 4/Source/MyApp/app/src/main/java/com/example/dell/myapp/Display.java
@@ -0,0 +1,83 @@
+package com.example.dell.myapp;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.speech.tts.TextToSpeech;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import java.util.Locale;
+
+public class Display extends Activity {
+
+    TextToSpeech ttsobject;
+    int result;
+    EditText et;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+        String username = getIntent().getStringExtra("Username");
+
+        TextView tv = (TextView)findViewById(R.id.TVusername);
+        tv.setText(username);
+
+        et = (EditText)findViewById(R.id.editText);
+
+        ttsobject = new TextToSpeech(Display.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if(status == TextToSpeech.SUCCESS){
+
+                    result = ttsobject.setLanguage(Locale.UK);
+
+                }else {
+
+                    Toast.makeText(getApplicationContext(), "Feature not Supported in your Device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void doSomething(View v) {
+
+        switch (v.getId()){
+            case R.id.bspeak:
+
+                if(result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA){
+
+                    Toast.makeText(getApplicationContext(), "Feature not Supported in your Device", Toast.LENGTH_SHORT).show();
+                } else {
+                    text = et.getText().toString();
+
+                    ttsobject.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+
+                }
+
+                break;
+            case R.id.bstopspeaking:
+
+                if(ttsobject != null) {
+                    ttsobject.stop();
+                }
+
+
+                break;
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(ttsobject != null) {
+            ttsobject.stop();
+            ttsobject.shutdown();
+        }
+    }
+}
---
+++ lab assignment 4/Source/MyApp/app/src/main/java/com/example/dell/myapp/SignUp.java
@@ -0,0 +1,92 @@
+package com.example.dell.myapp;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class SignUp extends Activity {
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void onSignUpClick(View v) {
+        if (v.getId() == R.id.Bsignupbutton) {
+            EditText name = (EditText) findViewById(R.id.TFname);
+            EditText email = (EditText) findViewById(R.id.TFemail);
+            EditText uname = (EditText) findViewById(R.id.TFuname);
+            EditText pass1 = (EditText) findViewById(R.id.TFpass1);
+            EditText pass2 = (EditText) findViewById(R.id.TFpass2);
+
+            String namestr = name.getText().toString();
+            String emailstr = email.getText().toString();
+            String unamestr = uname.getText().toString();
+            String pass1str = pass1.getText().toString();
+            String pass2str = pass2.getText().toString();
+
+            if (!pass1str.endsWith(pass2str)) {
+                //pop msg
+                Toast pass = Toast.makeText(SignUp.this, "Password doen't match!", Toast.LENGTH_SHORT);
+                pass.show();
+            }
+
+        }
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("SignUp Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
+
