98579a9a01ed26e0bb8a0e12f9570facc150c3d4
rbt2d <ramanakumarbingi@gmail.com>
2016-10-17 02:06:58 -0500

    updated

---
+++ lab assignment 7/source/ramana/www/js/controllers.js
@@ -0,0 +1,67 @@
+angular.module('app.controllers', [])
+  
+.controller('homePageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('appCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('locationCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+.controller('myCtrl', function($scope, $cordovaGeolocation){
+
+    $scope.toggle = function(){
+
+      var posOption = {timeout: 10000,enableHighAccuracy: true};
+      $cordovaGeolocation
+        .getCurrentPosition(posOption)
+        .then(function (position)  {
+
+            $scope.lat = position.coords.latitude;
+            $scope.long = position.coords.longitude;
+
+
+          }, function(err){
+            //error
+          }
+
+        );
+
+    }
+
+
+
+  })
+
+ 
